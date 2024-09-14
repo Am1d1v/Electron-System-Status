a0fe4ed05e266c41ab0688dc37f3899f3f8388b7
Am1d1v <dimadiman1adima@gmail.com>
2024-09-14 15:21:19 +0300

    Nav Toggle Menu Item

--- main.js
+++ main.js
@@ -103,6 +103,15 @@ const menu = [
   {
     role: 'fileMenu',
   },
+  {
+    label: 'View', 
+    submenu: [
+      {
+        label: 'Toggle Navigation',
+        click: () => mainWindow.webContents.send('nav:toggle')
+      }
+    ]
+  },
   ...(isDev
     ? [
         {
